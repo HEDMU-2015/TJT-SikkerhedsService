bdc8898afbb7b93973237a1c2f899aa1c0f81c2f
tsvetelin <tsvetelin@tsvetelin-pc.ceuherning.int>
2016-04-14 16:21:00 +0200

    ...

---
+++ src/organization/logic/OrganizationImpl.java
@@ -0,0 +1,42 @@
+package organization.logic;
+
+import java.util.List;
+
+import organization.api.Organization;
+import organization.domain.OrganizationUnit;
+
+public class OrganizationImpl implements Organization {
+
+	@Override
+	public OrganizationUnit getOrganizationUnit(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OrganizationUnit> getChildren(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Long> getChildrenIds(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OrganizationUnit> getAllOrganizationsWithoutParents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OrganizationUnit> searchOrganization(String search) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
