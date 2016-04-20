6d9246b5ffd8500aebbcb15244d17a37b4e688fa
tsvetelin <tsvetelin@tsvetelin-pc.ceuherning.int>
2016-04-20 08:44:46 +0200

    ...

--- junit/security/data/PermissionMapperForMysqlTest.java
+++ junit/security/data/PermissionMapperForMysqlTest.java
@@ -1,6 +1,6 @@
 package security.data;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.*; 
 
 import org.junit.Before;
 import org.junit.Test;
--- junit/security/data/UserMapperForMysqlImplTest.java
+++ junit/security/data/UserMapperForMysqlImplTest.java
@@ -1,6 +1,6 @@
 package security.data;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.*; 
 
 import org.junit.After;
 import org.junit.Before;
